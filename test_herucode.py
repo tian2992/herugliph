e032c31a4dad278fed6f279d0a6e2b97ed058041
tian <code@sebastianoliva.com>
2021-07-13 22:33:43 -0600

    test styb

---
+++ test_herucode.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+from herucode import HeruLang
+
+class TestHeruLang(TestCase):
+
+    def setUp(self) -> None:
+        self.HL_class = HeruLang
+
+    def test_is_preposition(self):
+        self.fail()
+
+    def test_is_verb(self):
+        self.fail()
+
+    def test_is_subjunctive_verb(self):
+        self.fail()
+
+    def test_as_number(self):
+        print(self.HL_class.as_number("gxjrc"))
+        self.fail()
+
+    def test_is_pretty(self):
+        self.fail()
+
+    def test_analyze(self):
+        self.fail()

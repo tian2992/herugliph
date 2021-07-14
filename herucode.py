31461a6d23874af9694b57d1475dde18b85f11e1
tian <code@sebastianoliva.com>
2021-07-13 22:57:42 -0600

    ok boomer

--- herucode.py
+++ herucode.py
@@ -1,4 +1,5 @@
 import math
+from collections import Counter
 
 
 class HeruLang:
@@ -6,30 +7,30 @@ class HeruLang:
     FOO_LETTERS = ["u", "d", "x", "s", "m", "p", "f"]
     BAR_LETTERS = list(set(ALPHABET) - set(FOO_LETTERS))  # List instead of set for consistency.
 
-    def __init__(self, word_list):
+    def __init__(self):
         pass
 
     @staticmethod
-    def is_preposition(self, word):
+    def is_preposition(word):
         return ((len(word) == 6) and
                 (word[-1] in HeruLang.FOO_LETTERS) and
                 ('u' not in word))
 
     @staticmethod
-    def is_verb(self, word):
+    def is_verb(word):
         return len(word) >= 6 and word[-1] in HeruLang.BAR_LETTERS
 
     @staticmethod
-    def is_subjunctive_verb(self, word):
-        return self.is_verb(word) and word[0] in HeruLang.BAR_LETTERS
+    def is_subjunctive_verb(word):
+        return HeruLang.is_verb(word) and word[0] in HeruLang.BAR_LETTERS
 
     @staticmethod
-    def _herulang_sort(self, word):
+    def _herulang_sort(word):
         """Generates sorting key for words on herulang alphabet."""
         return [HeruLang.ALPHABET.index(l) for l in word]
 
     @staticmethod
-    def as_number(self, word):
+    def as_number(word):
         """Parses numbers as vigesimal based on their position read from left to right."""
         index = 0
         digit_sum: int = 0
@@ -39,12 +40,30 @@ class HeruLang:
         return digit_sum
 
     @staticmethod
-    def is_pretty(self, number):
+    def is_pretty(number):
         """Checks if number is classified as 'pretty'."""
         return number >= 81827 and number % 3 == 0
 
     def analyze(self, full_text):
-        wordlist = full_text.split(" ")
-        ## TODO: remove dupes
+        full_wordlist = full_text.split()
+        recounted = Counter(full_wordlist)
+        wordlist = list(recounted.keys())
         sort_words = sorted(wordlist, key=self._herulang_sort)
-        return
+        count_verbs, count_prep, count_subj, count_pretty = 0, 0, 0, 0
+        for word in sort_words:
+            if self.is_preposition(word):
+                count_prep += 1
+            if self.is_verb(word):
+                count_verbs += 1
+            if self.is_subjunctive_verb(word):
+                count_subj += 1
+            if self.is_pretty(self.as_number(word)):
+                count_pretty += 1
+
+        return {
+            "sorted_words": sort_words,
+            "verbs": count_verbs,
+            "pretty_numbers": count_pretty,
+            "subjunctive_verbs": count_subj,
+            "prepositions": count_prep
+        }

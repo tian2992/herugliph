9f6c697405d3450482974bb4059fcf3b3c1373cb
tian <code@sebastianoliva.com>
2021-07-13 23:23:19 -0600

    v1

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 from herucode import HeruLang
 
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
@@ -13,15 +14,19 @@ def hello_world():
 
 @app.route('/parse_post', methods=['POST'])
 def parse_heru_post_arg():
-    word_list = request.values['text']
+    word_list = request.values.get('text', "")
     hl = HeruLang()
     return hl.analyze(word_list)
 
+
 @app.route('/parse', methods=['POST'])
 def parse_heru_post_json():
-    words = []  # TODO: load words as json arg value
-    hl = HeruLang(words)
-    return {}
+    if request.is_json:
+        words = request.json['text']
+    else:
+        words = ""
+    hl = HeruLang()
+    return hl.analyze(words)
 
 
 if __name__ == '__main__':
